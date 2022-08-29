ca621343ccacef8fd86edf9e5bf75e7f1e259039
堀巧 <horitakumi@horitakuminoMacBook-Pro.local>
2022-08-30 00:33:47 +0900

    update: find-all-anagrams-in-a-string.py

--- find-all-anagrams-in-a-string.py
+++ find-all-anagrams-in-a-string.py
@@ -1,31 +1,25 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        size_s, size_p = len(s), len(p)
-        if size_s < size_p:
-            return []
+        p_counters = [0] * 26
+        for letter in p:
+            letter_index = ord(letter) - ord('a')
+            p_counters[letter_index] += 1
 
-        result = []
-        window = len(p)
-        counts_p = self.getCountsOf(p)
-        counts_window_s = [0] * 26
+        window_counters = [0] * 26
+        p_size = len(p)
+        head = 0
+        results = []
+        for tail, tail_letter in enumerate(s):
+            # increment tail
+            tail_index = ord(tail_letter) - ord('a')
+            window_counters[tail_index] += 1
+            # decrement head
+            if tail+1 > p_size:
+                head_index = ord(s[head]) - ord('a')
+                window_counters[head_index] -= 1
+                head += 1
+            
+            if p_counters == window_counters:
+                results.append(head)
 
-        for i, letter in enumerate(s):
-            counts_window_s[self.getNumberOf(letter)] += 1
-            # windowの外になる分を引く
-            if i >= window:
-                counts_window_s[self.getNumberOf(s[i-window])] -= 1
-
-            if counts_p == counts_window_s:
-                result.append(i-window+1)
-
-        return result
-
-    def getCountsOf(self, string: str) -> List[int]:
-        counts = [0] * 26
-        for letter in string:
-            counts[self.getNumberOf(letter)] += 1
-
-        return counts
-
-    def getNumberOf(self, letter: str) -> int :
-        return ord(letter)-ord('a') # ordでunicodeの番号. -ord('a')でoffset
+        return results
